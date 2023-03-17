e57c8a15836d83cb662369d5aa27c0afabd53249
Salomon <salomonleon197@gmail.com>
2023-03-16 23:38:22 -0300

    reponsive navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import DrawerNav from "./DrawerNav";
 import menuIcon from "../images/icons/menu-icon.png";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 type headerProps = {
   fullname: string;
@@ -14,6 +14,18 @@ function Header({ fullname }: headerProps) {
     .map((word) => word[0])
     .join("");
 
+
+  useEffect(() => {
+
+    function handleResize() {
+      setIsOpen(false)
+    }
+    window.addEventListener('resize', handleResize)
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+
   function toggleNav() {
     setIsOpen((prevIsOpen) => !prevIsOpen);
   }
@@ -22,9 +34,18 @@ function Header({ fullname }: headerProps) {
     <header className="header">
       <p className="section__title">{initials}</p>
       <nav className="desk-nav">
-        <ul>
-          <li>
-            <a href="#projects">Projects</a>
+        <ul className="desk-nav__list">
+          <li className="desk-nav__item">
+            <a className="section__text" href="#about">About Me</a>
+          </li>
+          <li className="desk-nav__item">
+            <a className="section__text"  href="#skills">Skills</a>
+          </li>
+          <li className="desk-nav__item">
+            <a className="section__text" href="#projects">Projects</a>
+          </li>
+          <li className="desk-nav__item">
+            <a className="section__text" href="#contact">Contact Me</a>
           </li>
         </ul>
       </nav>
